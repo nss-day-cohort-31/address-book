de038783619f68094ceb26854df8523816472bfd
Jisie DM <jisie.david@gmail.com>
2019-03-07 09:42:38 -0600

    Using an array for addressBook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,34 @@
-// Some starter code
+let addressBook = [
+    {
+        name: "Jisie",
+        address: "5918 Brook Bend Dr"
+    },
+    {
+        name: "Kristen",
+        address: "452 Moss Trail"
+    },
+    {
+        name: "Steve",
+        address: "500 Interstate Blvd S"
+    }
+]
+
 const container = document.querySelector("#addressList")
+
+const listAddressOnDom = () => {
+    container.innerHTML = ""
+    debugger
+    addressBook.forEach(element => {
+    container.innerHTML += `
+        <section>
+            <h1>${element.name}</h1>
+            <div>${element.address}</div>
+        </section>
+    `
+    });
+}
+
+listAddressOnDom()
 
 document.querySelector("#saveEntry").addEventListener("click", event => {
     /*
@@ -9,13 +38,12 @@ document.querySelector("#saveEntry").addEventListener("click", event => {
     const personName = document.querySelector("#fullName").value
     const personAddress = document.querySelector("#address").value
 
-    // Once you have collected all the values, update the DOM
-    // with some HTML
-    container.innerHTML += `
-        <section>
-            <h1>${personName}</h1>
-            <div>${personAddress}</div>
-        </section>
-    `
+    let newAddressBookEntry = {
+        name: personName,
+        address: personAddress
+    }
+    // debugger
+    addressBook.push(newAddressBookEntry)
+    listAddressOnDom()
 
 })
